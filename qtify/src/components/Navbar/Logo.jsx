42d07806bbfb3226aa7892108b2d350cad218e3d
tanujas <tanujas@pharmarack.com>
2024-02-20 16:06:15 +0530

    test cases failure resolved changes

--- qtify/src/components/Navbar/Logo.jsx
+++ qtify/src/components/Navbar/Logo.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import LogoImage from "../../assets/logo.png"
+import styles from "./Logo.module.css";
 
 export default function Logo() {
     return (
-    <>
+    <div className={styles.logodiv}>
         <img src={LogoImage} alt="QTrip Logo" width={50} />
-    </>
+    </div>
     )
 }
--- qtify/src/components/Navbar/SearchBar.jsx
+++ qtify/src/components/Navbar/SearchBar.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as SearchButton } from "../../assets/search-icon.svg";
 export default function SearchBar() {
     return (
       <div className={styles.container}>
-        <input type="text" name="search" placeholder="Enter an album" className={styles.searchinput} />
+        <input type="text" name="search" placeholder="Search an album of your choice" className={styles.searchinput} />
         <button type="button" className={styles.searchButton}>
             <SearchButton />
         </button>
