cd36ec4f06dfa30bfdf18acfefd75fe421789b66
tanuja8707 <66427017+tanuja8707@users.noreply.github.com>
2024-03-02 13:36:37 +0530

    changes carousel

--- qtify/src/components/Navbar/Navbar.jsx
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import Button from "../Button/Button";
 import Search from "../Search/Search";
 import styles from './Navbar.module.css'
-import LogoImage from "../../assets/Logo.png"
+import Logo from "../../assets/Logo.png"
 
 
 export default function Navbar(){
     return(
         <>
             <nav className={styles.navbar}>
-                <div className={styles.logoDiv}><img  src={LogoImage} alt="logo" width={67}/></div>
+                <div className={styles.logoDiv}><img  src={Logo} alt="logo" width={67}/></div>
                  <Search search={"Search a song of your choice"}/>
                 <Button children="Give Feedback"/>
             </nav>
